3c9027e62d24877243bac67b582f09e935ad11bb
Scott and Cody Z <scott+codyz@appacademy.io>
2013-05-14 09:12:18 -0400

    Initial setup. Email and user tables.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hn::Application.config.secret_token = '1df59b23c0cb6b48909f94bf88e21ff382cb4af844f059c561376b48d124c7ee627a55081133d5e4ac40f20ee8517a90a32973230207ea024532104716be7175'
